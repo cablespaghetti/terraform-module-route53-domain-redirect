9629a1d60225d58edfaf9e1f62ff7e43c7b75fc5
Mubarak Jama <mubarak.jama@gmail.com>
2022-01-27 14:23:05 -0700

    only support tls1.2 for cloudfront

--- cloudfront.tf
+++ cloudfront.tf
@@ -7,7 +7,7 @@ resource "aws_cloudfront_distribution" "redirect" {
       http_port              = 80
       https_port             = 443
       origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
