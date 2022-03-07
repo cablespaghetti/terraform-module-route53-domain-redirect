efbf3fa5b68077301460667ba4a2c74c5214203c
Christopher Mersman <chris.mersman@ibotta.com>
2022-03-07 13:47:57 -0700

    Missing locals

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  domain                = redirect_to_subdomain ? var.origin_subdomain : var.zone
+  domain                = local.redirect_to_subdomain ? var.origin_subdomain : var.zone
   redirect_to_subdomain = var.origin_subdomain != ""
 }
