bd1205facdd33de70173c4d5401f0860cc45a0cb
Mubarak Jama <mubarak.jama@gmail.com>
2023-09-18 10:29:29 -0600

    fix missing s3 website config

--- s3.tf
+++ s3.tf
@@ -6,10 +6,6 @@ resource "random_string" "hash" {
 resource "aws_s3_bucket" "redirect_bucket" {
   bucket = "redirect-${local.domain}-${lower(random_string.hash.result)}"
 
-  website {
-    redirect_all_requests_to = var.target_url
-  }
-
   tags = var.tags
 
 }
@@ -45,4 +41,11 @@ resource "aws_s3_bucket_acl" "redirect_bucket" {
 
   bucket = aws_s3_bucket.redirect_bucket.id
   acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "redirect_bucket" {
+  bucket = aws_s3_bucket.redirect_bucket.id
+  redirect_all_requests_to {
+    host_name = var.target_url
+  }
 }
